6285a973ed19ae0346aa2ec9ec370a9777579081
Oak Atsume <relay@oakatsume.xyz>
2025-04-25 14:15:36 -0600

    Adjusted

--- main.rb
+++ main.rb
@@ -28,7 +28,7 @@ def randomWrap(text, inserts,params, strings)
     elsif word.match?(/\w/) && rand < 0.25
       "<a href=\"#{inserts.sample}?#{params.sample}=#{inserts.sample}\">#{word}</a>"
     elsif word.match?(/\w/) && rand < 0.15
-      "#{word} #{strings.sample}"
+      "#{word} <!-- #{strings.sample} -->"
     else
       word
     end
